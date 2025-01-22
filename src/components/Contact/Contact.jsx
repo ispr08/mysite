d04491c48198ea70f046fe65edcb5aab0026273c
IRAGAMREDDY SIVA PRASAD REDDY <85291768+ispr08@users.noreply.github.com>
2025-01-22 17:57:30 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,7 +13,7 @@ export const Contact = () => {
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:xmirack@gmail.com">xmirack@gmail.com</a>
+          <a href="mailto:ispr54@gmail.com">ispr54@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
@@ -24,7 +24,7 @@ export const Contact = () => {
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/zmirack">Github/zmirack</a>
+          <a href="https://www.github.com/zmirack">Github/ispr08</a>
         </li>
       </ul>
     </footer>
